53f75791ccefb31a0a07e201c901bc51cc8d38a9
Harini R K <harinixid@gmail.com>
2025-09-29 10:44:40 +0530

    Update dept.php
    updated dept

--- dept.php
+++ dept.php
@@ -295,7 +295,7 @@
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb mb-0">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">dept</li>
+                    <li class="breadcrumb-item active" aria-current="page">Department</li>
                 </ol>
             </nav>
         </div>
